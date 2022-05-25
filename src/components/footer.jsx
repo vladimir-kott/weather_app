b3f2cb50c6d147ff6d22017bc70cd47ae8e3bd31
Vladimir Kott <vladimirkott19@gmail.com>
2022-05-25 16:49:07 +0300

    fix bugs

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return ( 
-        <footer className="lg:fixed md:flex justify-center inset-x-0 bottom-0 p-3 bg-slate-300 shadow-md mt-3">
+        <footer className="fixed text-center inset-x-0 bottom-0 p-3 bg-slate-300 shadow-md mt-3">
             <p className="text-white text-sm">by Vladimir Kott (25.05.2022)</p>
         </footer>
      );
